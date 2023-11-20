18d4b791b67d0dc8ee7c5a9b6ffd2f204b9cf788
RARITTO <meenalalit18803@gmail.com>
2023-11-20 21:50:56 +0530

    Update notification.py

--- notification.py
+++ notification.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         title="Please Drink Alcohol Daily",#heading data 
         message="It makes you happy and also provides relief from the pain of anxiety, so please drink alcohol daily, time to time.", #what kind of data your wanted to enter 
         app_icon= "C:\Users\Lalit\Downloads\beer.ico", # choose the icon location carefullu
-        timeout=10
+        timeout=15
     )
 
 print('You got the notification successfully')
