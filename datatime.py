545018aada689be9ccd84a709ea59d1d39c91c4d
RARITTO <meenalalit18803@gmail.com>
2023-11-28 22:06:04 +0530

    game (rock,paper,scissor)

--- datatime.py
+++ datatime.py
@@ -1,7 +1,25 @@
-from datetime import datetime
+import socket
 
+# Create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-current_datetime = datetime.now()
+# Bind the socket to a specific address and port
+server_address = ('127.0.0.1', 5555)
+server_socket.bind(server_address)
 
+# Listen for incoming connections (max 5 connections in the queue)
+server_socket.listen(5)
 
-print("Current Date and Time:", current_datetime)
+print(f"Server is listening on {server_address}")
+
+while True:
+    # Wait for a connection
+    client_socket, client_address = server_socket.accept()
+    print(f"Received connection from {client_address}")
+
+    # Send a welcome message to the client
+    message = "Welcome to the server!"
+    client_socket.send(message.encode('utf-8'))
+
+    # Close the connection
+    client_socket.close()
