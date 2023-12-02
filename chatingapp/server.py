35220967af41442545b45cc25ece19d42ca93d23
RARITTO <meenalalit18803@gmail.com>
2023-12-02 12:25:42 +0530

    bug fixed

--- chatingapp/server.py
+++ chatingapp/server.py
@@ -1,21 +1,25 @@
 import socket
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("localhost", 9773))
-server.listen()
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(("localhost", 9773))
+server_socket.listen()
+print('server is listening to 9773 ')
 
-client, address = server.accept()
 
-done = False
 
-while not done:
-    msg = client.recv(1024).decode('utf-8')
+while True :
+    
+    client_socket, client_address = server_socket.accept()
+    print(f'accept from {client_address}')
+    
+    msg = client_socket.recv(1024).decode('utf-8')
     if msg == 'quit':
-        done = True
+        client_socket.close()
     else:
         print(msg)
 
-        response = input('message: ')
-        client.send(response.encode('utf-8'))
+        # response = input('message: ')
+        # client_socket.send(response.encode('utf-8'))
+    client_socket.close()
 
-client.close()
+# client_socket.close()
