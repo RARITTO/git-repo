a7a388ed69315b07705ce1dabb7cd2211d7a2801
RARITTO <meenalalit18803@gmail.com>
2023-12-02 11:56:00 +0530

    chat

--- chatingapp/client.py
+++ chatingapp/client.py
@@ -2,7 +2,7 @@ import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server.bind(("localhost", 9772))
+# server.bind(("localhost", 9774))
 server.listen()
 
 client, address = server.accept()
